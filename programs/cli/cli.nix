fac9f737d38a6fb8e4b520f54ed0e91c10653100
Guillaume Fournier <me@guif.dev>
2026-04-29 10:20:47 +0200

    feat: add nix-index-database

--- programs/cli/cli.nix
+++ programs/cli/cli.nix
@@ -1,10 +1,24 @@
 {
+  inputs,
+  ...
+}:
+{
+
   flake.modules.homeManager.cli =
     { pkgs, ... }:
     {
 
+      imports = [
+        inputs.nix-index-database.homeModules.default
+      ];
+
       programs = {
         zoxide.enable = true;
+        # This will still use the nix-index-database
+        nix-index.enable = true;
+        nix-index-database = {
+          comma.enable = true;
+        };
         direnv = {
           enable = true;
           nix-direnv.enable = true;
