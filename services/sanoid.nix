2519f747a7744e732b082f1daf0c4e92f93868fd
Guillaume Fournier <me@guif.dev>
2026-05-25 09:11:03 +0200

    feat: make sanoid pools configurable

--- services/sanoid.nix
+++ services/sanoid.nix
@@ -4,29 +4,39 @@
 {
   flake.modules.nixos.sanoid =
     { lib, config, ... }:
-    {
-      services.sanoid = {
-        enable = true;
-        interval = "hourly";
-        datasets.tank = {
-          hourly = 72;
-          daily = 30;
-          weekly = 4;
-          monthly = 6;
-          autosnap = true;
-          autoprune = true;
-          recursive = true;
-          processChildrenOnly = true;
-        };
+    let
+      cfg = config.mine.zfs;
+      datasetConfig = {
+        hourly = 72;
+        daily = 30;
+        weekly = 4;
+        monthly = 6;
+        autosnap = cfg.sanoid.autosnap;
+        autoprune = true;
+        recursive = true;
+        processChildrenOnly = true;
       };
-      users = lib.mkIf config.services.sanoid.enable {
-        users = {
-          sanoid = {
+    in
+    {
+      options.mine.zfs.sanoid.autosnap = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether sanoid should automatically create snapshots. Disable on syncoid receive targets.";
+      };
+
+      config = lib.mkIf (cfg.pools != [ ]) {
+        services.sanoid = {
+          enable = true;
+          interval = "hourly";
+          datasets = lib.genAttrs cfg.pools (_: datasetConfig);
+        };
+        users = lib.mkIf config.services.sanoid.enable {
+          users.sanoid = {
             group = "sanoid";
             isSystemUser = true;
           };
+          groups.sanoid = { };
         };
-        groups.sanoid = { };
       };
     };
 }
--- system/settings/zfs.nix
+++ system/settings/zfs.nix
@@ -1,11 +1,25 @@
 { inputs, ... }:
 
 {
-  flake.modules.nixos.zfs = {
-    imports = with inputs.self.modules.nixos; [ sanoid ];
-    boot.supportedFilesystems = [ "zfs" ];
-    boot.zfs.extraPools = [ "tank" ];
-    services.zfs.autoScrub.enable = true;
-    services.zfs.autoScrub.interval = "monthly";
-  };
+  flake.modules.nixos.zfs =
+    { lib, config, ... }:
+    let
+      cfg = config.mine.zfs;
+    in
+    {
+      imports = with inputs.self.modules.nixos; [ sanoid ];
+
+      options.mine.zfs.pools = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        description = "ZFS pools to import and manage.";
+      };
+
+      config = lib.mkIf (cfg.pools != [ ]) {
+        boot.supportedFilesystems = [ "zfs" ];
+        boot.zfs.extraPools = cfg.pools;
+        services.zfs.autoScrub.enable = true;
+        services.zfs.autoScrub.interval = "monthly";
+      };
+    };
 }
