866797ba9531210425e420316653947f8bc9f2f3
Guillaume Fournier <me@guif.dev>
2026-06-18 14:33:54 +0200

    feat: add python libs for kicad addons

--- programs/wm/cad.nix
+++ programs/wm/cad.nix
@@ -8,7 +8,18 @@
       home.packages = with pkgs; [
         cura-appimage
         freecad
-        kicad
+        # kicad with extra Python packages available to plugins (scipy, shapely).
+        # overrideAttrs extends the pythonPath list used by wrapPython, which
+        # builds the PYTHONPATH that kicad sets on all its wrapped binaries.
+        (kicad.overrideAttrs (old: {
+          pythonPath =
+            old.pythonPath
+            ++ (with python3Packages; [
+              # Used for KiCad Routing tools
+              scipy
+              shapely
+            ]);
+        }))
         easyeda2kicad
         temurin-jre-bin # Needed for kicads plugin freerouting
       ];
